b32871f1c70cb2c2b409a2d40277b4eb872f886b
Sash3nce7o <126651154+Sash3nce7o@users.noreply.github.com>
2023-12-07 14:46:10 +0200

    Add files via upload

---
+++ Game of War/Game of War/CardSuit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_War
+{
+    public enum CardSuit
+    {
+        Spade = '♠',
+        Club = '♣',
+        Heart = '♥',
+        Diamond = '♦'
+    }
+}
